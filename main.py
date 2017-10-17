63f7cd7a5b620c6847b32af55a94bbff17f7c004
Maclav <maclav3@gmail.com>
2017-10-18 00:41:28 +0200

    Package get_default_logger into a Loggable class

--- main.py
+++ main.py
@@ -5,14 +5,17 @@ from sys import argv
 
 import coloredlogs
 
-_logger = None
 
+class Loggable(object):
+    @classmethod
+    def get_default_logger(cls):
+        logger = getattr(cls, '_default_logger', None)
+        if not logger:
+            logger = logging.getLogger(__name__)
+            coloredlogs.install('DEBUG', logger=logger)
+            cls._default_logger = logger
 
-def get_default_logger():
-    if not _logger:
-        logger = logging.getLogger(__name__)
-        coloredlogs.install('DEBUG', logger=logger)
-    return _logger
+        return logger
 
 
 if __name__ == '__main__':
