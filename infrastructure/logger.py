ad26019cb7b9bb72d58a89a4b2a22c1d40aafffd
Maclav <maclav3@gmail.com>
2017-10-18 00:49:12 +0200

    Remove unnecessary shebangs; move logger to dedicated package

---
+++ infrastructure/logger.py
@@ -0,0 +1,15 @@
+import logging
+
+import coloredlogs
+
+
+class Loggable(object):
+    @classmethod
+    def get_default_logger(cls):
+        logger = getattr(cls, '_default_logger', None)
+        if not logger:
+            logger = logging.getLogger(__name__)
+            coloredlogs.install('DEBUG', logger=logger)
+            cls._default_logger = logger
+
+        return logger
--- main.py
+++ main.py
@@ -1,22 +1,6 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-import logging
 from sys import argv
-
-import coloredlogs
-
-
-class Loggable(object):
-    @classmethod
-    def get_default_logger(cls):
-        logger = getattr(cls, '_default_logger', None)
-        if not logger:
-            logger = logging.getLogger(__name__)
-            coloredlogs.install('DEBUG', logger=logger)
-            cls._default_logger = logger
-
-        return logger
-
 
 if __name__ == '__main__':
     print(argv[0])
